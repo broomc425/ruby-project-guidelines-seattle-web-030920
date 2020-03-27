d554bec18ffcb3d1a092880350863d6ca3b8677a
Charlie Broom <56619462+broomc425@users.noreply.github.com>
2020-03-26 23:02:49 -0700

    Delete seeds.rb

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# User.destroy_all
-# Team.destroy_all
-
-# jordan = User.create(username: "Michael Jordan")
-# bulls = Team.create(team_name: "Chicago Bulls")
-
-# jordan.teams << bulls
-
-# puts "CHICAGO BULLS!"
